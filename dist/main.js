cd207ae50eeb1d5ab76ef90465501f6d3e7ab95f
mitchspiron <mitchspiron280@gmail.com>
2022-09-11 11:05:10 +0300

    fix: Cors error

--- dist/main.js
+++ dist/main.js
@@ -6,6 +6,16 @@ const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.useGlobalPipes(new common_1.ValidationPipe());
+    app.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+        next();
+    });
+    app.enableCors({
+        allowedHeaders: '*',
+        origin: '*',
+    });
     await app.listen(process.env.PORT);
     console.log('🚀 Server started at http://127.0.0.1:' + process.env.PORT);
 }
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,19 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
+
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+  });
+
   await app.listen(process.env.PORT);
   console.log('🚀 Server started at http://127.0.0.1:' + process.env.PORT);
 }
